e932f6157bfe550ec923c3ec2ffed76310b8a19d
fwcd <fwcd@fwcd.dev>
2024-05-16 17:00:33 +0200

    Import numpy.typing as npt
    As per the convention:
    https://numpy.org/doc/stable/reference/typing.html#module-numpy.typing

--- minq/ops.py
+++ minq/ops.py
@@ -1,10 +1,10 @@
 from functools import reduce
-from numpy.typing import ArrayLike, NDArray
 from typing import Any, cast
 
 from minq.utils import from_binary
 
 import numpy as np
+import numpy.typing as npt
 
 def ket(*args: int | str):
     '''Creates a standard basis vector'''
@@ -21,12 +21,12 @@ def ket(*args: int | str):
         x[int(args[0], base=2)] = 1
         return x
 
-def kron(*arrays: ArrayLike) -> NDArray[Any]:
+def kron(*arrays: npt.ArrayLike) -> npt.NDArray[Any]:
     '''Computes the Kronecker product (tensor product) of the given arrays'''
     assert len(arrays) > 0
     return reduce(np.kron, arrays)
 
-def kronpow(array: ArrayLike, n: int) -> NDArray[Any]:
+def kronpow(array: npt.ArrayLike, n: int) -> npt.NDArray[Any]:
     '''Applies the Kronecker product n times'''
     assert n > 0
     return reduce(lambda acc, _: np.kron(acc, array), range(n - 1), array)
